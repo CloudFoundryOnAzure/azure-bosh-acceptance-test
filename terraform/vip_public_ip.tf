eb625bba715d96f7315debf10b63e6bbade7436e
Bin Xia <binxi@microsoft.com>
2018-10-15 16:21:27 +0000

    Add scenarios for manual/dynamic/vip network

---
+++ terraform/vip_public_ip.tf
@@ -0,0 +1,10 @@
+resource "azurerm_public_ip" "vip" {
+  name                         = "vip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.bosh_resource_group.name}"
+  public_ip_address_allocation = "static"
+}
+
+output "vip_address" {
+  value = "${azurerm_public_ip.vip.ip_address}"
+}
