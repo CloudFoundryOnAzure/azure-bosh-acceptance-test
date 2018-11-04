206514001db24bf26bdf7e8b599d19286b6244f3
Bin Xia <binxi@microsoft.com>
2018-11-04 07:14:20 +0000

    Create containers in storage accounts

--- terraform/constants.tf
+++ terraform/constants.tf
@@ -1,7 +1,3 @@
 data "template_file" "base_storage_account_wildcard" {
   template = "boshvms"
 }
-
-data "template_file" "stemcells_table" {
-  template = "stemcells"
-}
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -6,8 +6,22 @@ resource "azurerm_storage_account" "default_storage_account" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "bosh_storage_container" {
+  name                  = "bosh"
+  resource_group_name   = "${azurerm_resource_group.bosh_resource_group.name}"
+  storage_account_name  = "${azurerm_storage_account.default_storage_account.name}"
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "stemcell_storage_container" {
+  name                  = "stemcell"
+  resource_group_name   = "${azurerm_resource_group.bosh_resource_group.name}"
+  storage_account_name  = "${azurerm_storage_account.default_storage_account.name}"
+  container_access_type = "private"
+}
+
 resource "azurerm_storage_table" "stemcells_storage_table" {
-  name                  = "${data.template_file.stemcells_table.rendered}"
+  name                  = "stemcells"
   resource_group_name   = "${azurerm_resource_group.bosh_resource_group.name}"
   storage_account_name  = "${azurerm_storage_account.default_storage_account.name}"
 }
@@ -18,6 +32,26 @@ resource "azurerm_storage_account" "bosh_vms_storage_account" {
   location                 = "${var.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  count = 2
+}
+
+resource "azurerm_storage_container" "bosh_vms_storage_container" {
+  name                  = "bosh"
+  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account"]
+  resource_group_name   = "${azurerm_resource_group.bosh_resource_group.name}"
+  storage_account_name  = "${element(azurerm_storage_account.bosh_vms_storage_account.*.name, count.index)}"
+  container_access_type = "private"
+
+  count = 2
+}
+
+resource "azurerm_storage_container" "bosh_vms_stemcell_storage_container" {
+  name                  = "stemcell"
+  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account"]
+  resource_group_name   = "${azurerm_resource_group.bosh_resource_group.name}"
+  storage_account_name  = "${element(azurerm_storage_account.bosh_vms_storage_account.*.name, count.index)}"
+  container_access_type = "private"
 
   count = 2
 }
